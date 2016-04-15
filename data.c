a82b33ae0d9ad6924f5f02424384ec277121ffef
Jonas Lima da Silva <jonaslimads@gmail.com>
2016-04-14 22:13:18 -0300

    Load any data record based on its record number.

--- data.c
+++ data.c
@@ -1,44 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct entry {
+typedef struct record {
 	char vcod[7];
 	char produtor[24];
 	char cepa[15];
 	int colheita;
 	char pais[15];
-} entry;
+} record;
 
-void write_data(entry *entries, int num_entries, FILE *f) {
-	fwrite(entries, sizeof(entry), num_entries, f);
+void write_data(record *records, int num_records, FILE *f) {
+	fwrite(records, sizeof(record), num_records, f);
+}
+
+void load_data(FILE *f, long offset) {
+	record r;
+
+	fseek(f, offset, SEEK_SET);
+	fread(&r, sizeof(r), 1, f);
+
+	printf("%s\n", r.vcod);
 }
 
 int main() {
 
-	int num_entries = 2;
+	int num_records = 3;
 	
-	entry e[2] = {
+	record r[3] = {
 		{"V100", "Don Laurindo", "Merlot", 2011, "Brasil"},
-		{"V150", "Chateau Lafitte", "Chardonnay", 2010, "Franca"}
+		{"V150", "Chateau Lafitte", "Chardonnay", 2010, "Franca"},
+		{"V175", "Chryseia", "Douro", 2014, "Portugal"}
 	};
 
 	// write
 	FILE *f;
 	f = fopen("Vinhos.dat", "wb+");
 
-	// printf("%1ld\n", sizeof(e));
+	// printf("%1ld\n", sizeof(r));
 
-	write_data(e, num_entries, f);
+	write_data(r, num_records, f);
 	fclose(f);
 
 	// read
 	FILE* in;
-	entry e_;
+	record r_;
 
 	in = fopen("Vinhos.dat", "rb+");
-	fread(&e_, sizeof(e_), 1, in);
 
-	printf("%s\n", e_.vcod);
+	load_data(f, 0*sizeof(record)); // first record
+	load_data(f, 1*sizeof(record)); // second...
+	load_data(f, 2*sizeof(record)); // 
+
+	// printf("%s\n", r_.vcod);
 
 	fclose(in);
 
